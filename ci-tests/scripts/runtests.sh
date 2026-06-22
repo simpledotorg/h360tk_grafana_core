352d08fde909385b80035637214d9c0f3100ee89
Arnaud <ademarcq@rtsl.org>
2026-06-22 18:34:23 +0200

    Now generates usefull report from json

--- ci-tests/scripts/runtests.sh
+++ ci-tests/scripts/runtests.sh
@@ -1,7 +1,14 @@
 ## APK
 apt-get update
 apt-get upgrade
-apt-get install -y --no-install-recommends libpq5 allure 
+apt-get install -y --no-install-recommends libpq5 allure openjdk-21-jre-headless nodejs npm wget unzip 
+
+wget -q https://github.com/allure-framework/allure2/releases/download/2.30.0/allure-2.30.0.zip -O /tmp/allure.zip
+unzip -q /tmp/allure.zip -d /opt/
+ln -sf /opt/allure-2.30.0/bin/allure /usr/bin/allure
+rm /tmp/allure.zip
+
+
 
 ## Pip
 pip install --upgrade pip
@@ -12,4 +19,3 @@ cd /tests/
 behave --junit --junit-directory=/target -f allure_behave.formatter:AllureFormatter -o /target/allure-results
 
 allure generate /target/allure-results --clean -o /target/html-report
-
